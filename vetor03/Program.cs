d323483d9cc80fb92e122a6886b700cdd28e148f
Felipe Speranza <felipesperanza@hotmail.com>
2020-02-05 13:57:58 -0300

    Exercicios c#

---
+++ vetor03/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace vetor03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int[] a;
+            int[] b;
+            int[] c;
+
+            n = int.Parse(Console.ReadLine());
+            string[] s = Console.ReadLine().Split(' ');
+            string[] ss = Console.ReadLine().Split(' ');
+            a = new int[n];
+            b = new int[n];
+            c = new int[n];
+
+
+
+
+            //gravar dados vetor a
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = int.Parse(s[i]);
+            }
+
+            //gravar dados vetor b
+            for (int i = 0; i < n; i++)
+            {
+                b[i] = int.Parse(ss[i]);
+            }
+
+            //soma vetores
+            for (int i = 0; i < n; i++)
+            {
+                c[i] = a[i] + b[i];
+            }
+
+            //mostrar vetor
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(c[i] + " ");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ vetor04/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace vetor04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            double[] vet;
+
+            n = int.Parse(Console.ReadLine());
+            vet = new double[n];
+
+            string[] s = Console.ReadLine().Split(' ');
+
+            //gravar dados
+            for (int i = 0; i < n; i++)
+            {
+                vet[i] = double.Parse(s[i]);
+            }
+
+            //media aritmética
+            double media = 0;
+            for (int i = 0; i < n; i++)
+            {
+                media = media + vet[i];
+            }
+            media = media / n;
+
+            Console.WriteLine(media.ToString("F3", CultureInfo.InvariantCulture));
+
+            for (int i = 0; i < n; i++)
+            {
+                if (vet[i] < media)
+                {
+                    Console.WriteLine(vet[i]);
+                }
+            }
+
+
+
+        }
+    }
+}
