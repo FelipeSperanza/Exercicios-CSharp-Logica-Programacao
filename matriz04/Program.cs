7b602c20574476f88901f3980f3ced184b03a8df
Felipe Speranza <felipesperanza@hotmail.com>
2020-02-07 02:26:42 -0300

    Exercicios c#

---
+++ matriz04/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace matriz04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int[,] matriz;
+
+            n = int.Parse(Console.ReadLine());
+
+            matriz = new int[n, n];
+
+            for(int i = 0; i<n; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                for(int j = 0; j<n; j++)
+                {
+                    matriz[i, j] = int.Parse(s[j]);
+                }
+            }
+
+
+            int soma = 0;
+            for(int i = 0; i<n; i++)
+            {
+                for (int j = 0; j<n; j++)
+                {
+                    if(j > i)
+                    {
+                        soma = soma + matriz[i, j];
+                    }
+                }
+            }
+
+            Console.WriteLine(soma);
+
+        }
+    }
+}
