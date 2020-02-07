8ac2b417c8371b26b63dd11b43ff498c8e9be3b0
Felipe Speranza <felipesperanza@hotmail.com>
2020-02-07 13:52:45 -0300

    Exercicios C#

--- matriz06/Program.cs
+++ matriz06/Program.cs
@@ -25,6 +25,9 @@ namespace matriz06
                 }
             }
 
+            linha = int.Parse(Console.ReadLine());
+            coluna = int.Parse(Console.ReadLine());
+
             //somar positivos
             double soma = 0;
             for(int i = 0; i<n; i++)
@@ -38,9 +41,64 @@ namespace matriz06
                 }
             }
 
-            Console.WriteLine(soma.ToString("F1",CultureInfo.InvariantCulture));
+            Console.WriteLine("SOMA DOS POSITIVOS: " + soma.ToString("F1", CultureInfo.InvariantCulture));
+
+            //imprimir linha
+            Console.Write("LINHA ESCOLIDA ");
+            for(int i = linha; i<=linha; i++)
+            {
+                for(int j = 0; j<n; j++)
+                {
+                    Console.Write(matriz[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            //imprimir coluna
+            Console.Write("COLUNA ESCOLIDA ");
+            for (int i = 0; i<n; i++)
+            {
+                for(int j = coluna; j <= coluna; j++)
+                {
+                    Console.Write(matriz[i,j].ToString("F1", CultureInfo.InvariantCulture) + " ");
+                }
+            }
+            Console.WriteLine();
+
+            //Diagonal principal
+            Console.Write("DIAGONAL PRINCIPAL: ");
+            for(int i = 0; i < n; i++)
+            {
+                for(int j =0; j<n; j++)
+                {
+                    if (i == j)
+                    {
+                        Console.Write(matriz[i,j].ToString("F1", CultureInfo.InvariantCulture) + " ");;
+                    }
+                }
+            }
+
+            Console.WriteLine();
+
+            Console.Write("MATRIZ INVERTIDA: ");
+            Console.WriteLine();
+
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j<n; j++)
+                {
+                    if(matriz[i,j] < 0.0)
+                    {
+                        matriz[i,j] = Math.Pow(matriz[i, j], 2);
+                    }
+                    Console.Write(matriz[i, j].ToString("F1", CultureInfo.InvariantCulture) + " ");
+                }
+                
+                Console.WriteLine();
+            }
 
 
+            Console.WriteLine();
         }
     }
 }
